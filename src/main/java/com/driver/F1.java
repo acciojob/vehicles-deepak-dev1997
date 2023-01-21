98a48bbaac063b9bddc22a51b3ba2564638de838
Deepak_Dev <deepak.devdd.97@gmail.com>
2023-01-21 17:33:33 +0530

    sol3

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -24,18 +24,21 @@ public class F1 extends Car {
             changeGear(1);
         }
         else if(newSpeed>=1 && newSpeed<=50){
-            changeGear(2);
+            changeGear(1);
         }
         else if(newSpeed>=51 && newSpeed<=100){
-            changeGear(3);
+            changeGear(2);
         }
         else if(newSpeed>=101 && newSpeed<=150){
-            changeGear(4);
+            changeGear(3);
         }
         else if(newSpeed>=151 && newSpeed<=200){
-            changeGear(5);
+            changeGear(4);
         }
         else if(newSpeed>=201 && newSpeed<=250){
+            changeGear(5);
+        }
+        else if(newSpeed>250){
             changeGear(6);
         }
         //for all other cases, change the gear accordingly
